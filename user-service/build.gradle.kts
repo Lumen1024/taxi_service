8d47229c5a5dd37755f5f7ce54db4ef2cb1b8774
Lumen1024 <lumenhorizon@icloud.com>
2026-05-10 01:23:04 +0700

    work

--- user-service/build.gradle.kts
+++ user-service/build.gradle.kts
@@ -3,8 +3,11 @@ plugins {
 }
 
 dependencies {
+    implementation(project(":common"))
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-webmvc")
     implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
     runtimeOnly("org.postgresql:postgresql")
 }
